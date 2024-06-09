c640bb67ae9be659ce3c1c6acd384c5e9b451321
mariama coly <mariamacoly739@gmail.com>
2024-06-09 21:02:13 +0000

    commit

---
+++ script.js
@@ -0,0 +1,32 @@
+/*var age;
+
+age = 34;*/
+// var age = 14;
+// document.write(age);
+
+// var age = 14;
+// alert(age);
+
+// var age = 14;
+// age = 28;
+// document.write(age);
+
+// var age = 14;
+// document.write(age);
+// age = 'jason';
+// document.write(age);
+
+// var age = 14;
+// document.write(typeof age);
+
+// var name = prompt("comment t'appelles-tu ?");
+// document.write(name);
+
+// var age = prompt("quel age as-tu ?");
+// age = parseFloat(age);
+// document.write(typeof age);
+
+// les constantes
+
+const PI = 3.14;
+document.write(PI);
