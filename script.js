2a4b73d468daa1008c9cb60b071f56073e8b4ca3
mariama coly <mariamacoly739@gmail.com>
2024-06-09 21:16:44 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/*let i = 0;
+while(i < 10)
+    {
+        document.write("a");
+        i++;
+    }
+*/
+/*let i = 10;
+do
+{
+    document.write("a");
+    i++;
+}
+while(i < 10);
+*/
+/*
+for(let i = 0 ; i < 5 ; ++i)
+{
+    document.write("a");
+}
+*/
+
+for(let i = 0 ; i < 5 ; ++i)
+{
+    document.write("a");
+    document.write("b ");
+}
+
