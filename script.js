34760b524cb54c0086deab0b5e5c43fe6a8722b6
mariama coly <mariamacoly739@gmail.com>
2024-06-09 21:06:15 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// let result = 14;
+// document.write(result);
+
+// document.write(5 / 2);
+
+// let N = 5;
+// N = N + 1;
+// document.write(N)
+
+// let N = 5;
+// document.write(N++);
+
+// let N = 5;
+// document.write(++N);
+
+// document.write(8 > 12);
+
+// document.write(8 === 8);
+
+// document.write(8 === '8');
+
+// document.write(8 != 16);
+
+document.write(8 !== "8");
