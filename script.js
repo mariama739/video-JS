7b01e9af89b20d89c85cff67c9579b57703e19d8
mariama coly <mariamacoly739@gmail.com>
2024-06-09 21:13:33 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// let name = "Yuna";
+
+// if(name === "Yuna")
+// {
+//     document.write("OUI");
+//     let number = 6;
+
+//     if(number === 6)
+//     document.write("oui");
+// }
+// else
+// {
+//     document.write("NON");
+// }
+
+/*let number = 6;
+if(number < 0)
+    {
+    document.write("< 0");
+    }
+else if(number > 30)
+    {
+    document.write("> 10");
+    }
+else
+ {
+    document.write("number");
+ }
+*/
+
+let number = 4;
+switch(number)
+{
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        document.write("number = 5")
+}
